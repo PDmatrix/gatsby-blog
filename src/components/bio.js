0f4ee069474f24585dd88064b83c14d303babf22
PDmatrix <dimasebakovgoo@gmail.com>
2019-03-29 03:12:48 +0300

    Upload profile pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,67 +1,32 @@
-/**
- * Bio component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import profilePic from '../../content/assets/profile-pic.jpg';
 
 import { rhythm } from "../utils/typography"
 
 function Bio() {
   return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
-            }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-            <p>
-              Written by <strong>{author}</strong>
-            </p>
-          </div>
-        )
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
       }}
-    />
+    >
+      <img
+        alt={"Dmitry Sebakov"}
+        src={profilePic}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          width: rhythm(2),
+          height: rhythm(2.15),
+          borderRadius: "50%",
+        }}
+      />
+      <p>
+        Written by <strong>Dmitry Sebakov</strong>
+      </p>
+    </div>
   )
 }
-
-const bioQuery = graphql`
-  query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    site {
-      siteMetadata {
-        author
-      }
-    }
-  }
-`
 
 export default Bio
