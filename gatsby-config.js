696bb07d8445ba02c3177a6580ba437432f9e399
PDmatrix <dimasebakovgoo@gmail.com>
2019-04-08 22:09:57 +0300

    Custom html, sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Dmatrix's thoughts`,
     author: `Dmitry Sebakov`,
     description: `Personal blog for my thoughts`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://www.dmatrix.dev/`,
   },
   plugins: [
     {
@@ -39,28 +39,9 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
               aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in src/layouts/index.js
-              // right after importing the prism color scheme:
-              //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
               showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false,
             },
           },
@@ -70,6 +51,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -84,8 +66,8 @@ module.exports = {
         name: `Dmatrix's blog`,
         short_name: `Blog`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
+        background_color: `#282a36`,
+        theme_color: `#282a36`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
