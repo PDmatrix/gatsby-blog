103dec6e09aea338b1e905aac8c4bee3c3d64445
PDmatrix <dimasebakovgoo@gmail.com>
2019-04-08 20:11:48 +0300

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-137936524-1`,
       },
     },
     `gatsby-plugin-feed`,
